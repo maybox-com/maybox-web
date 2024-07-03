82ce01fe02441f27c470c51e8f823ac5909eb8c0
aaronteamAAR <rooneychris83@gmail.com>
2024-07-03 14:36:52 +0100

    added fonts to tailwind

--- maybox-web/tailwind.config.js
+++ maybox-web/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ordinary: ['Ordinary', 'sans-serif'],
+        neueEinstellung: ['Neue Einstellung', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: 100,
+        extraLight: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semiBold: 600,
+        bold: 700,
+        extraBold: 800,
+        black: 900,
+      },
+    },
   },
   plugins: [],
 }
