05ef16fe6dde76d374922c91bda88a04c01e58fe
aaronteamAAR <rooneychris83@gmail.com>
2024-07-07 13:23:17 +0100

    Put about page in the right place

--- maybox-web/src/components/Aboutbody.js
+++ maybox-web/src/components/Aboutbody.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaStar, FaRegHeart } from "react-icons/fa";
 // import { Link } from 'react-router-dom';
-import './css/Aboutbody.css';
+import '../assets/styles/Aboutbody.css';
 
 const Aboutbody = () => {
   return (
