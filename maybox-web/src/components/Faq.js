9e2857420b6c47de7ee99a29a40e61fe5988ebad
aaronteamAAR <rooneychris83@gmail.com>
2024-07-07 19:33:30 +0100

    85%

--- maybox-web/src/components/Faq.js
+++ maybox-web/src/components/Faq.js
@@ -9,108 +9,199 @@ function Faq() {
   };
   return (
     <>
-      <section className="py-24">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="flex flex-col justify-center items-center gap-x-16 gap-y-5 xl:gap-28 lg:flex-row lg:justify-between max-lg:max-w-2xl mx-auto max-w-full">
-            {/* <div className="w-full lg:w-1/2">
-              <img
-                src="https://pagedone.io/asset/uploads/1696230182.png"
-                alt="FAQ tailwind section"
-                className="w-auto"
-              />
-            </div> */}
-           <div class="space-y-4">
-  <details class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden" open>
-    <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
-      <h2 class="font-medium">Lorem ipsum dolor sit amet consectetur adipisicing?</h2>
-
-      <span class="relative size-5 shrink-0">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="absolute inset-0 size-5 opacity-100 group-open:opacity-0"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="absolute inset-0 size-5 opacity-0 group-open:opacity-100"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      </span>
-    </summary>
-
-    <p class="mt-4 leading-relaxed text-gray-700">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis molestias culpa in,
-      recusandae laboriosam neque aliquid libero nesciunt voluptate dicta quo officiis explicabo
-      consequuntur distinctio corporis earum similique!
-    </p>
-  </details>
-
-  <details class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
-    <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
-      <h2 class="font-medium">Lorem ipsum dolor sit amet consectetur adipisicing?</h2>
-
-      <span class="relative size-5 shrink-0">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="absolute inset-0 opacity-100 group-open:opacity-0"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="absolute inset-0 opacity-0 group-open:opacity-100"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      </span>
-    </summary>
-
-    <p class="mt-4 leading-relaxed text-gray-700">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis molestias culpa in,
-      recusandae laboriosam neque aliquid libero nesciunt voluptate dicta quo officiis explicabo
-      consequuntur distinctio corporis earum similique!
-    </p>
-  </details>
-</div>
-          </div>
+      <div class="space-y-4 px-2 md:px-[10em]">
+        <div className="font-playtip text-center my-10 text-3xl mb: text-[54px]">
+          <h1>Questions?Answers.</h1>
         </div>
-      </section>
+              <details
+                class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+                open
+              >
+                <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+                  <h2 class="font-medium font-playtip">
+                    Lorem ipsum dolor sit amet consectetur adipisicing?
+                  </h2>
+
+                  <span class="relative size-5 shrink-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="absolute inset-0 size-5 opacity-100 group-open:opacity-0"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="absolute inset-0 size-5 opacity-0 group-open:opacity-100"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </span>
+                </summary>
+
+                <p class="mt-4 leading-relaxed text-gray-700 font-neueEinstellung">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab
+                  hic veritatis molestias culpa in, recusandae laboriosam neque
+                  aliquid libero nesciunt voluptate dicta quo officiis explicabo
+                  consequuntur distinctio corporis earum similique!
+                </p>
+              </details>
+
+              <details class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+                <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+                  <h2 class="font-medium font-playtip">
+                    Lorem ipsum dolor sit amet consectetur adipisicing?
+                  </h2>
+
+                  <span class="relative size-5 shrink-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="absolute inset-0 opacity-100 group-open:opacity-0"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="absolute inset-0 opacity-0 group-open:opacity-100"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </span>
+                </summary>
+
+                <p class="mt-4 leading-relaxed text-gray-700 font-neueEinstellung">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab
+                  hic veritatis molestias culpa in, recusandae laboriosam neque
+                  aliquid libero nesciunt voluptate dicta quo officiis explicabo
+                  consequuntur distinctio corporis earum similique!
+                </p>
+              </details>
+              <details class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+                <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+                  <h2 class="font-medium font-playtip">
+                    Lorem ipsum dolor sit amet consectetur adipisicing?
+                  </h2>
+
+                  <span class="relative size-5 shrink-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="absolute inset-0 opacity-100 group-open:opacity-0"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="absolute inset-0 opacity-0 group-open:opacity-100"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </span>
+                </summary>
+
+                <p class="mt-4 leading-relaxed text-gray-700 font-neueEinstellung">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab
+                  hic veritatis molestias culpa in, recusandae laboriosam neque
+                  aliquid libero nesciunt voluptate dicta quo officiis explicabo
+                  consequuntur distinctio corporis earum similique!
+                </p>
+              </details>
+              <details class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+                <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+                  <h2 class="font-medium font-playtip">
+                    Lorem ipsum dolor sit amet consectetur adipisicing?
+                  </h2>
+
+                  <span class="relative size-5 shrink-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="absolute inset-0 opacity-100 group-open:opacity-0"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="absolute inset-0 opacity-0 group-open:opacity-100"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </span>
+                </summary>
+
+                <p class="mt-4 leading-relaxed text-gray-700 font-neueEinstellung">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab
+                  hic veritatis molestias culpa in, recusandae laboriosam neque
+                  aliquid libero nesciunt voluptate dicta quo officiis explicabo
+                  consequuntur distinctio corporis earum similique!
+                </p>
+              </details>
+            </div>
     </>
   );
 }
--- maybox-web/src/components/Plans.js
+++ maybox-web/src/components/Plans.js
@@ -6,27 +6,27 @@ function Plans() {
     return ( 
         <>
          <Steps />
-       <div className='plan-container flex flex-col justify-center md:flex-row-reverse px-4 md:px-[12em] space-y-8 md:space-y-0 md:space-x-8 rtl:space-x-reverse'>
-      <div className='main-container justify-center mt-10'>
+       <div className='plan-container flex flex-col justify-center border-b-2 md:flex-row-reverse px-4 md:px-[12em] space-y-8 md:space-y-0 md:space-x-8 rtl:space-x-reverse'>
+      <div className='main-container justify-center my-[5.4em]'>
         <div className='pricing-txt text-center'>
-          <h1 className='font-playtip text-3xl md:text-[54px] '>How to get started ?</h1>
+          <h1 className='font-playtip mb-6 text-3xl md:text-[54px] '>How to get started ?</h1>
           <span className='font-neueEinstellung text-[18px] md:text-[24px]'>Select a simple subscription plan to set you up</span>
         </div>
-        <div className="price-container flex flex-col md:flex-row justify-center items-center gap-8 md:gap-[5em] mt-8">
+        <div className="price-container flex flex-col  md:flex-row justify-center items-center gap-8 md:gap-[5em] mt-8">
   <div className="price-plans font-neueEinstellung text-center md:text-left">
-    <h1 className="font-semibold text-2xl text-customOrangeDark">Standard</h1>
-    <span className="font-bold">$30/m USD</span>
+    <h1 className="font-semibold  text-3xl text-customOrangeDark md:text-2xl">Standard</h1>
+    <span className="font-bold text-center">$30/m USD</span>
     <span>Pause and cancel anytime</span>
     <button className="plans-btn bg-customOrangeDark mt-4">Subscribe</button>
   </div>
   <div className="price-plans font-neueEinstellung text-center md:text-left">
-    <h1 className="font-semibold text-2xl text-customOrangeDark">Premium</h1>
+    <h1 className="font-semibold text-3xl text-customOrangeDark md:text-2xl">Premium</h1>
     <span className="font-bold">$50/m USD</span>
     <span>Pause and cancel anytime</span>
     <button className="plans-btn bg-customOrangeDark mt-4">Subscribe</button>
   </div>
   <div className="price-plans font-neueEinstellung text-center md:text-left">
-    <h1 className="font-semibold text-2xl text-customOrangeDark">Pro</h1>
+    <h1 className="font-semibold text-3xl text-customOrangeDark md:text-2xl">Pro</h1>
     <span className="font-bold">$70/m USD</span>
     <span>Pause and cancel anytime</span>
     <button className="plans-btn bg-customOrangeDark mt-4">Subscribe</button>
