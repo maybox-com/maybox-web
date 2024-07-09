3fb75d92e1e33b67c509f59fbc214d34a8b5174e
ChinememmaObilor29 <89047128+ChinememmaObilor29@users.noreply.github.com>
2024-07-09 12:15:37 +0100

    newly updated
    aboutpage

--- maybox-web/src/App.js
+++ maybox-web/src/App.js
@@ -1,20 +1,28 @@
 import Body from './components/Body';
 import Navbar from './components/Navbar';
+<<<<<<< Updated upstream
 
 import Home from './pages/Home'
 import About from './pages/About';
 
 import Home from './pages/Home';
 
+=======
+>>>>>>> Stashed changes
 import './index.css';
-import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className='bg-img'>
     <div className="container mx-auto scroll-behavior: smooth; ">
      <Home />
+=======
+    <div className="App">
+      <Navbar />
+     <Body />
+>>>>>>> Stashed changes
     </div>
      </div>
   );
--- maybox-web/src/components/Aboutbody.js
+++ maybox-web/src/components/Aboutbody.js
@@ -19,88 +19,86 @@ const Aboutbody = () => {
         </div>
       </section>
 
-      <section className='bg-gray-100 mt-24'>
-        <div className="px-4">
-          <div className="relative flex flex-col lg:flex-row p-4 md:p-12">
-            <div className="flex-1 mb-8 lg:mb-0">
-              <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold text-customRed">
-                Unveiling Wellness <br />
-                <span>Excellence</span>
-              </h2>
-            </div>
-            <div className="flex-1">
-              <p className="text-base sm:text-lg md:text-xl font-medium">
-                With Maybox, discover the transforming realm of wellness, beauty, and self-care. Our carefully chosen collections provide tailored answers that enable you to succeed in all facets of your life. Every product, from opulent skincare to revitalizing self-care practices, is chosen based on its effectiveness and quality. With Maybox, you can embrace wellness since each item is designed to accentuate the beauty of nature and encourage a sense of well-being. Learn the true meaning of self-care and improve your everyday regimen with items that yield remarkable outcomes.
-              </p>
-            </div>
+      <section className="mt-24">
+        <div className='relative flex flex-col lg:flex-row'>
+          <div className='w-full lg:w-1/2'>
+            <img
+              src='https://vimcosmo.com/pics/10-years-experience.webp'
+              alt=''
+              className='rounded-r-full hover:animate-pulse w-full lg:w-auto'
+            />
           </div>
-          <div className="flex justify-center p-4 md:p-12">
-            <div className="max-w-full">
-              <p className="text-center text-base sm:text-lg md:text-xl px-4 md:px-36">
-                From the inception of our lifestyle health app to the dynamic team leading its development, our story is one of enthusiasm, dedication, and ongoing development. We are a community, not just a team, motivated by the same goal of transforming the future of well-being. Discover our path, where each accomplishment, obstacle, and milestone adds to a legacy of excellence and empowerment for all in the field of holistic wellness.
-              </p>
-            </div>
+
+          <div className='ml-0 lg:ml-24 mt-12 lg:mt-24 p-4 lg:p-0'>
+            <h2 className="text-4xl sm:text-5xl lg:text-6xl font-medium mb-8 text-customPink">
+              Love your beauty, <br />
+              <span className="mt-4 inline-block">body & skin</span>
+            </h2>
+            <p className='text-lg sm:text-xl lg:text-2xl text-gray-400 font-semibold mb-8'>
+              Try something new by exploring our website <br />
+              and we will help you find what works for you <br />
+              and your skin, with your top concerns in mind
+            </p>
+
+            <Link to="#" className="flex justify-center items-center bg-red-200 text-customRed rounded-full hover:bg-customRed hover:text-white transition duration-300 text-lg sm:text-xl lg:text-2xl font-medium py-4 lg:py-6 w-full lg:w-1/2 hover:-translate-y-2 hover:scale-120">
+              Browse products
+            </Link>
           </div>
         </div>
       </section>
 
-      <section className='bg-gray-100'>
+      <section className='bg-gray-100 mt-24'>
         <div className="px-4">
-          <div>
-            <div className='flex items-center p-8 space-x-2'>
-              <FaStar className='text-customRed text-5xl' />
-              <h2 className='font-bold text-3xl sm:text-4xl md:text-5xl'>Our Vision</h2>
-            </div>
+          <div className='flex items-center p-8 space-x-2'>
+            <FaStar className='text-customRed text-5xl' />
+            <h2 className='font-bold text-3xl sm:text-4xl md:text-5xl'>Our Vision</h2>
           </div>
 
           <div className='flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8 px-4 md:px-11'>
             <div className='mb-8 md:mb-16'>
               <p className='text-2xl sm:text-3xl text-gray-400 font-medium'>01.</p>
-              <h3 className='mt-2 font-bold text-lg sm:text-xl'>Deliver Personalized beauty product discoveries:</h3>
-              <hr className="border-t border-gray-900 my-2" />
+              <h3 className='my-4 font-bold text-lg sm:text-xl'>Deliver Personalized beauty product discoveries:</h3>
               <p className='text-base'>Maybox aims to curate and send its subscribers a monthly box filled with deluxe samples of high-quality beauty, skincare, and grooming products tailored to each individual's unique preferences and needs.</p>
             </div>
 
             <div className='mb-8 md:mb-16'>
               <p className='text-2xl sm:text-3xl text-gray-400 font-medium'>02.</p>
-              <h3 className='mt-2 font-bold text-lg sm:text-xl'>Deliver Personalized beauty product discoveries:</h3>
-              <hr className="border-t border-gray-900 my-2" />
+              <h3 className='my-4 font-bold text-lg sm:text-xl'>Deliver Personalized beauty product discoveries:</h3>
               <p className='text-base'>Maybox aims to curate and send its subscribers a monthly box filled with deluxe samples of high-quality beauty, skincare, and grooming products tailored to each individual's unique preferences and needs.</p>
             </div>
 
             <div className='mb-8 md:mb-16'>
               <p className='text-2xl sm:text-3xl text-gray-400 font-medium'>03.</p>
-              <h3 className='mt-2 font-bold text-lg sm:text-xl'>Deliver Personalized beauty product discoveries:</h3>
-              <hr className="border-t border-gray-900 my-2" />
+              <h3 className='my-4 font-bold text-lg sm:text-xl'>Deliver Personalized beauty product discoveries:</h3>
               <p className='text-base'>Maybox aims to curate and send its subscribers a monthly box filled with deluxe samples of high-quality beauty, skincare, and grooming products tailored to each individual's unique preferences and needs.</p>
             </div>
           </div>
         </div>
       </section>
 
-      <section className='bg-gray-100 sect4 mt-24'>
-        <div className="relative flex flex-col items-center justify-center min-h-screen px-4">
-          <div className="relative container mx-auto z-10 text-center">
-            <h1 className="text-4xl sm:text-5xl md:text-6xl font-ordinary font-bold text-customPink">
+      <section className='mt-24'>
+        <div className='relative flex flex-col lg:flex-row'>
+          <div className='ml-0 lg:ml-60 mt-12 lg:mt-24 p-4 lg:p-0'>
+            <h2 className="text-4xl sm:text-5xl lg:text-6xl font-medium mb-8 text-customPink">
               Unlock your<br />
-              <span className="relative inline-block font-ordinary font-bold sm:text-6xl md:text-7xl">
-                Beauty
-              </span>
-            </h1>
-
-            <p className="mt-8 text-base sm:text-lg md:text-xl font-neueEinstellung text-center">
-              Join Maybox today and experience a well-pleasing and stress-free joy.
+              <span className="mt-4 inline-block">body & skin</span>
+            </h2>
+            <p className='text-lg sm:text-xl lg:text-2xl text-gray-400 font-semibold mb-8'>
+              Join Maybox to enjoy a relaxing, enjoyable experience.
             </p>
 
-            <div className="mt-8 flex justify-center space-x-4">
-              <Link
-                to="#"
-                className="flex items-center px-6 sm:px-8 py-3 sm:py-4 bg-red-200 text-customRed rounded-full hover:bg-red-400 transition duration-300 text-lg sm:text-xl md:text-2xl"
-              >
-                <FaRegHeart className="mr-2 sm:mr-4 heart text-2xl sm:text-3xl rounded-full" />
-                Start Free Trial
-              </Link>
-            </div>
+            <Link to="#" className="flex justify-center items-center px-6 py-4 lg:py-6 bg-red-200 text-customRed rounded-full hover:bg-red-400 transition duration-300 w-full lg:w-1/2 font-medium hover:-translate-y-1 hover:scale-180">
+              <FaRegHeart className="mr-2 text-2xl heart lg:text-3xl rounded-full" />
+              <span className="hover:text-white text-lg lg:text-xl">Start Free Trial</span>
+            </Link>
+          </div>
+
+          <div className='w-full lg:w-1/2 ml-0 lg:ml-96 mt-12 lg:mt-0 p-4 lg:p-0'>
+            <img
+              src='https://img.freepik.com/free-photo/lovely-young-afro-woman-applies-nourishing-clay-mask-has-eyes-shut-lips-folded-covers-half-face-with-big-green-leaf-wears-shower-headband-isolated-pink-wall_273609-32987.jpg?t=st=1720435168~exp=1720438768~hmac=13b1e98a6f7a6822818eae9877134d390d43643ab476f0028de21e8339e1ea73&w=1380'
+              alt=''
+              className='rounded-l-full hover:animate-pulse w-full lg:w-auto'
+            />
           </div>
         </div>
       </section>
