324ab2c853510e492648cf5ac89bb318536d4410
aaronteamAAR <rooneychris83@gmail.com>
2024-09-14 15:50:44 +0100

    Forgotten page logic

--- maybox-web/src/components/Pages/ForgottenPass.js
+++ maybox-web/src/components/Pages/ForgottenPass.js
@@ -1,45 +1,79 @@
-import React, { useState } from 'react';
-import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import React, { useState, useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
 const ForgotPassword = () => {
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [timer, setTimer] = useState(0); 
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false); 
 
+  
   const handleForgotPassword = (e) => {
     e.preventDefault();
     const auth = getAuth();
 
     sendPasswordResetEmail(auth, email)
       .then(() => {
-        setMessage('Password reset email sent successfully. Please check your inbox.');
-        setError('');  
+        toast.success(
+          "Password reset email sent successfully. Please check your inbox."
+        );
+        setTimer(120); 
+        setIsButtonDisabled(true); 
       })
       .catch((err) => {
-        setError(err.message);
-        setMessage('');  
+        toast.error(`Error: ${err.message}`);
       });
   };
 
+
+  useEffect(() => {
+    let interval;
+    if (timer > 0) {
+      interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+    } else if (timer === 0) {
+      setIsButtonDisabled(false); 
+    }
+
+    return () => clearInterval(interval); 
+  }, [timer]);
+
   return (
-    <div className="forgot-password-container">
-      <h2>Forgot Your Password?</h2>
-      <form onSubmit={handleForgotPassword}>
-        <label htmlFor="email">Enter your email address:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          className="email-input"
-        />
-        <button type="submit" className="submit-button">
-          Send Reset Email
-        </button>
+    <div className="flex justify-center items-center mt-28 flex-col min-h-screen">
+      <Toaster position="top-center" reverseOrder={false} />
+      <h2 className="font-bold font-playtip text-4xl text-center mb-5">
+        Forgot Your Password?
+      </h2>
+      <form
+        onSubmit={handleForgotPassword}
+        className="flex flex-col justify-center items-center w-full"
+      >
+        <div className="flex justify-center items-center min-h-screen">
+          <div className="w-full max-w-xs p-5 bg-white rounded-lg font-neueEinstellung self-start">
+            <label
+              className="block text-gray-700 text-xl font-medium mb-2"
+              htmlFor="unique-input"
+            >
+              Enter your email address
+            </label>
+            <input
+              className="text-sm custom-input w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform hover:shadow-lg hover:border-blue-300 bg-gray-100"
+              placeholder="Email address here"
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+            />
+            <button
+              type="submit"
+              className="submit-button mt-4 flex rounded-xl edit3 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              disabled={isButtonDisabled}
+            >
+              {timer > 0 ? `Resend in ${timer}s` : "Send Reset Email"} 
+            </button>
+          </div>
+        </div>
       </form>
-      {message && <p className="success-message">{message}</p>}
-      {error && <p className="error-message">{error}</p>}
     </div>
   );
 };
--- maybox-web/src/pages/ForgottenPassword.js
+++ maybox-web/src/pages/ForgottenPassword.js
@@ -1,10 +1,12 @@
 import ForgotPassword from "../components/Pages/ForgottenPass";
+import Footer from '../components/Layout/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Authentication</h1>
       <ForgotPassword />
+      <Footer />
     </div>
   );
 }
